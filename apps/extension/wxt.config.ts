9adeac0b55b394fc3836cd3d23ca6ca432d58f25
velhrh <valentyn.yurchenko@sysgears.com>
2026-08-29 18:01:34 +0300

    feat(extension): apply Lexync branding (#31)

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -8,6 +8,19 @@ export default defineConfig({
     return {
       name: 'Lexync',
       description: 'Deliberately capture private language-learning material.',
+      icons: {
+        16: 'icons/icon-16.png',
+        32: 'icons/icon-32.png',
+        48: 'icons/icon-48.png',
+        128: 'icons/icon-128.png',
+      },
+      action: {
+        default_icon: {
+          16: 'icons/icon-16.png',
+          24: 'icons/icon-24.png',
+          32: 'icons/icon-32.png',
+        },
+      },
       permissions: ['activeTab', 'scripting', 'storage', 'tabs'],
       host_permissions: [
         'http://*/*',
